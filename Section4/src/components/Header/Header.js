5fe23c1397726344df4f78795da7dc99853ed259
CJH <wlsgur831@gmail.com>
2023-08-28 00:21:49 +0900

    test

--- Section4/src/components/Header/Header.js
+++ Section4/src/components/Header/Header.js
@@ -1,8 +1,12 @@
-function Header(){}
-    return  <header>
-        
-         img src={keyConceptsImage} alt="Medal badge with a star" />
-        <h1>Key React Concepts</h1>
-        <p>Selected key React concepts you should know about</p>
-      </header>
+import keyConceptsImage from '../../assets/images/key-concepts.png';
+
+function Header() {
+  return (
+    <header>
+      <img src={keyConceptsImage} alt="Medal badge with a star" />
+      <h1>Key React Concepts</h1>
+      <p>Selected key React concepts you should know about</p>
+    </header>
+  )
+}
 export default Header;
