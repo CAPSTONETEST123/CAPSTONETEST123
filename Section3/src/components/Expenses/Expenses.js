cd639b75ce09d0fda5da913708af22bad29c2a23
choijinhyeok <wisgur831@naver.com>
2023-08-30 11:39:28 +0900

    0830

--- Section3/src/components/Expenses/Expenses.js
+++ Section3/src/components/Expenses/Expenses.js
@@ -1,9 +1,9 @@
 import React, { useState } from "react";
 
-import ExpenseItem from "./ExpenseItem";
 import Card from "../UI/Card";
 import "./Expenses.css";
 import ExpensesFilter from "./ExpensesFilter";
+import ExpenseList from "./ExpensesList";
 
 const Expenses = (props) => {
   const [filteredYear, setFilteredYear] = useState("2020");
@@ -16,22 +16,16 @@ const Expenses = (props) => {
   });
 
   return (
-    <Card className="expenses">
-      <ExpensesFilter
-        selected={filteredYear}
-        onChangeFilter={filterChangeHandler}
-      />
-
-      {/* 데이터 매핑하기 -> 하드코딩을 지양 함으로 map()을 이용해서 노출시킴 */}
-      {filtereExpenses.map((expense) => (
-        <ExpenseItem
-          key={expense.id}
-          title={expense.title}
-          amount={expense.amount}
-          date={expense.date}
+    <li>
+      <Card className="expenses">
+        <ExpensesFilter
+          selected={filteredYear}
+          onChangeFilter={filterChangeHandler}
         />
-      ))}
-    </Card>
+
+        <ExpenseList items={filtereExpenses} />
+      </Card>
+    </li>
   );
 };
 
